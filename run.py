080ebacdf21ad3de85367c6eed2693311b7481d1
AnnaDobrucki <anna.dobrucki@hotmail.co.uk>
2023-04-13 15:04:41 +0000

    add import of random and selected words for hangman

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+WORDS = ("python", "javascript", "needed", "carrying", "answer",  "piano")
+word = random.choice(WORDS)
+print(word)

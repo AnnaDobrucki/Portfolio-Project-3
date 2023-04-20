fdad883c9c94aec7c27cf4e4bc5214ca2d862998
AnnaDobrucki <anna.dobrucki@hotmail.co.uk>
2023-04-20 09:46:29 +0000

    connected up google sheets API with project to allow scoreboard access

--- run.py
+++ run.py
@@ -1,4 +1,22 @@
 import random
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+CREDS = Credentials.from_service_account_file('Creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('Hangman_P_3')
+
+score = SHEET.worksheet("Scoreboard")
+
+data = score.get_all_values()
+print(data)
+
 
 def pick_random_word():
     WORDS = ("python", "javascript", "needed", "carrying", "answer", "celestial", "piano")

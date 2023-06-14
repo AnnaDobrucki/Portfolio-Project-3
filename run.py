d1cf2d56bdc6a559a1429f293cf317304e07fddb
AnnaDobrucki <anna.dobrucki@hotmail.co.uk>
2023-06-14 07:12:57 +0000

    add notes for functions and statements for easier reading for others

--- run.py
+++ run.py
@@ -65,10 +65,8 @@ def pick_random_word():
     word = random.choice(WORDS)
     return word.upper()
 
-"""print(hidden_word)"""
-"""REMOVE AFTER BUILDING PLAYGAME FUNC"""
-
 def replay():
+    ### Replay function to allow user to end or try again with game ###
     while True:
         try:
             replay = input(" \nWould you like to try again? Enter Y/N").upper()
@@ -87,6 +85,7 @@ def replay():
                 print(value_error)
 
 def play_game(hidden_word):
+    ### Function to start playing the game and show the length of the hidden word###
     picked_word = list(hidden_word)
     print("The length of the word is below")
     print(len(hidden_word) * "_ ")
@@ -105,7 +104,7 @@ def play_game(hidden_word):
                     raise ValueError ("\n Fraid not, you already tried that!")
                     time.sleep(2)
                     clear_screen()
-
+            ### Checks user answers and iterrates through to see if word has been completed ###
                 elif answers in picked_word:
                     print(f"\n Smashing it out the park {username}! KEEP GOING! \n ")
                     guessed.append(answers)
@@ -118,7 +117,7 @@ def play_game(hidden_word):
                     word_arragement = "".join(hidden_word_list)
                     if "_" not in word_arragement:
                         tries = True
-                    
+            ### Checks if user entered wrong letter and keeps track of lives, until == 0 ###        
                 elif answers not in hidden_word:
                     guessed.append(answers)
                     lives_remaining -= 1
@@ -130,7 +129,7 @@ def play_game(hidden_word):
                 raise ValueError("Hang on we need only one letter, and remember no numbers either!")
         except ValueError as value_error:
                 print(value_error)
-        
+        ### Printing the answers onto screen for user ###
         current = ""
         if tries is False:
             for letter in hidden_word:
@@ -140,7 +139,7 @@ def play_game(hidden_word):
                     current += " _"
             print(current)
             
-        
+    ### If statement for win/ loose outcomes ###
     if tries:
         clear_screen()
         user_win()
